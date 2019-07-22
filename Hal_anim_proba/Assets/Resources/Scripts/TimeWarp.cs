6a38dfff38c804a43f67cf5a2ce0213ec8a6cd6d
lacmacfactac <andras.laszlo.halak@gmail.com>
2019-07-22 13:37:48 +0200

    Initial commit

---
+++ Hal_anim_proba/Assets/Resources/Scripts/TimeWarp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWarp : MonoBehaviour
+{
+    public float maximumTimeSpeed = 5;
+    public float minimumTimeSpeed = 1;
+    Animator[] animatorList;
+    float rawSpeed = 1;
+    float speed = 1;
+    bool updated = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorList = GameObject.FindObjectsOfType<Animator>();
+    }
+
+
+    public float Speed
+    {
+        get
+        {
+            return rawSpeed;
+        }
+
+        set
+        {
+            if (value != rawSpeed)
+            {
+                rawSpeed = value;
+                speed = Mathf.Lerp(minimumTimeSpeed, maximumTimeSpeed, rawSpeed);
+                foreach (Animator controller in animatorList)
+                {
+                    controller.speed = speed;
+                }
+            }
+        }
+    }
+}
---
+++ Hal_anim_proba/Assets/Resources/Scripts/VRInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRInput : MonoBehaviour
+{
+    public SteamVR_ActionSet actionset;
+    public SteamVR_Input_Sources inputSource;
+    public SteamVR_Action_Single timeWarpInput;
+
+    TimeWarp timeWarp;
+
+    float warpRaw = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeWarp = gameObject.GetComponent<TimeWarp>();
+
+        actionset.Activate();
+        timeWarpInput[inputSource].onChange += OnWarpChanged;
+        
+    }
+
+    private void OnWarpChanged(SteamVR_Action_Single fromAction, SteamVR_Input_Sources fromSource, float newAxis, float newDelta)
+    {
+        Debug.Log("New value: " + newAxis.ToString());
+        warpRaw = newAxis;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeWarp.Speed = warpRaw;
+    }
+}
